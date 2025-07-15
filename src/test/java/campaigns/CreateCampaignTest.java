e2d4ec62e33c338dc865b49666bf8662b1e4ad2c
sumivenk <42695914+sumivenk@users.noreply.github.com>
2025-07-15 07:21:11 +0530

    Update CreateCampaignTest.java

--- src/test/java/campaigns/CreateCampaignTest.java
+++ src/test/java/campaigns/CreateCampaignTest.java
@@ -45,7 +45,7 @@ public class CreateCampaignTest extends Base{
 		//Push back
 		//Pull2
 		
-		//test HEllo
+		//test Hello world
 		
 //		Login to vtiger application->mouseOverOn more Link->click on campaigns->click on create campaign lookup image->Enter campaignName->
 //		click on save Btn->verify whether the campaign is created in campaign Information page and Logout from the application. 
